957bded1d539aff555f4dc26e479b0fcb66bb7b2
AleksejVoronkin <120055748+AleksejVoronkin@users.noreply.github.com>
2023-01-18 22:41:36 +0300

    Add files via upload

---
+++ w25.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+
+Console.Clear();
+
+Console.Write("Введите число возводимое в степень: ");
+double a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+double b = Convert.ToInt32(Console.ReadLine());
+double s=a;
+//double s=Math.Pow(a,b); так можно, но это не цикл, а программа? for цикл?
+for (int i=1; i<b; i++)
+{
+    s=s*a;
+}
+Console.Write(a + "^" + b + "=" + s);
